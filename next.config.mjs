d6d640b7809ea670f0f26496a6305049ef7d447a
01guiCL <01guicostalopes@gmail.com>
2026-09-04 17:18:32 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/Fy',
+  assetPrefix: '/Fy/',
 };
 
 export default nextConfig;
